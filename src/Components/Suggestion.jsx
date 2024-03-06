61094562d0cf94b8a131359e1a6cb3e88590802f
Ashwani-Singh63 <ashwanisingh638796@gmail.com>
2024-03-06 22:17:23 +0530

    Adding suggestion part

--- src/Components/Suggestion.jsx
+++ src/Components/Suggestion.jsx
@@ -1,8 +1,163 @@
 import React from 'react'
+import Ashwani_pic from '../assets/image/ashwani-01.jpg'
 
 const Suggestion = () => {
   return (
-    <div className='h-[100vh] w-[25%] '>
+
+    <div className='h-[100vh] w-[25%] py-8'>
+
+      <div className='flex justify-start items-center mt-4 gap-8 pl-2'>
+
+        <img src={Ashwani_pic} className='w-[50px] h-[50px] rounded-[50%] ' alt="Profile Pic" />
+
+        <div className='flex flex-col justify-center'>
+
+            <p className='font-bold'>ashwani__singh63</p>
+
+            <p>Ashwani Singh</p>
+
+        </div>
+
+        <div className='flex justify-center items-center text-sky-500 hover:text-violet-500 cursor-pointer font-bold'>
+
+          Switch
+
+        </div>
+
+      </div>
+
+      <div className='flex justify-start items-center mt-4 gap-8 pl-2'>
+
+        <div className='flex flex-col justify-center'>
+
+            <p className='font-bold'>Suggested for you</p>
+
+           
+
+        </div>
+
+        <div className='flex justify-center items-center cursor-pointer font-bold'>
+
+          See All
+
+        </div>
+
+      </div>
+
+      <div className='flex justify-start items-center mt-4 gap-8 pl-2'>
+
+        <img src={Ashwani_pic} className='w-[50px] h-[50px] rounded-[50%] ' alt="Profile Pic" />
+
+        <div className='flex flex-col justify-center'>
+
+            <p className='font-bold'>ashwani__singh63</p>
+
+            <p>Ashwani Singh</p>
+
+        </div>
+
+        <div className='flex justify-center items-center text-sky-500 hover:text-violet-500 cursor-pointer font-bold'>
+
+          Follow
+
+        </div>
+
+      </div>
+
+      <div className='flex justify-start items-center mt-4 gap-8 pl-2'>
+
+        <img src={Ashwani_pic} className='w-[50px] h-[50px] rounded-[50%] ' alt="Profile Pic" />
+
+        <div className='flex flex-col justify-center'>
+
+            <p className='font-bold'>ashwani__singh63</p>
+
+            <p>Ashwani Singh</p>
+
+        </div>
+
+        <div className='flex justify-center items-center text-sky-500 hover:text-violet-500 cursor-pointer font-bold'>
+
+          Follow
+
+        </div>
+
+      </div>
+
+      <div className='flex justify-start items-center mt-4 gap-8 pl-2'>
+
+        <img src={Ashwani_pic} className='w-[50px] h-[50px] rounded-[50%] ' alt="Profile Pic" />
+
+        <div className='flex flex-col justify-center'>
+
+            <p className='font-bold'>ashwani__singh63</p>
+
+            <p>Ashwani Singh</p>
+
+        </div>
+
+        <div className='flex justify-center items-center text-sky-500 hover:text-violet-500 cursor-pointer font-bold'>
+
+          Follow
+
+        </div>
+
+      </div>
+
+      <div className='flex justify-start items-center mt-4 gap-8 pl-2'>
+
+        <img src={Ashwani_pic} className='w-[50px] h-[50px] rounded-[50%] ' alt="Profile Pic" />
+
+        <div className='flex flex-col justify-center'>
+
+            <p className='font-bold'>ashwani__singh63</p>
+
+            <p>Ashwani Singh</p>
+
+        </div>
+
+        <div className='flex justify-center items-center text-sky-500 hover:text-violet-500 cursor-pointer font-bold'>
+
+          Follow
+
+        </div>
+
+      </div>
+
+      <div className='flex justify-start items-center mt-4 gap-8 pl-2'>
+
+        <img src={Ashwani_pic} className='w-[50px] h-[50px] rounded-[50%] ' alt="Profile Pic" />
+
+        <div className='flex flex-col justify-center'>
+
+            <p className='font-bold'>ashwani__singh63</p>
+
+            <p>Ashwani Singh</p>
+
+        </div>
+
+        <div className='flex justify-center items-center text-sky-500 hover:text-violet-500 cursor-pointer font-bold'>
+
+          Follow
+
+        </div>
+
+      </div>
+
+      <div className='ml-2 mt-8'>
+
+        <p>About . Help . Press . API . Jobs . Privacy . Terms ,</p>
+
+        <p>Locations . Language . Meta Verified</p>
+        
+
+      </div>
+
+      <div className='ml-2 mt-8'>
+
+        <p>&copy; 2024 INSTAGRAM FROM META</p>
+
+      </div>
       
     </div>
   )
